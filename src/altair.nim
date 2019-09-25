08ef585f71c362cf99a3bf1a49ec6408eb145c93
t-sin <shinichi.tanaka45@gmail.com>
2019-09-25 22:35:15 +0900

    Super saw like sounds

--- src/altair.nim
+++ src/altair.nim
@@ -8,7 +8,8 @@ var
   saw1 = Saw(phase: 0, freq: 440)
   saw2 = Saw(phase: 0, freq: 445)
   env = Env(adsr: Release, eplaced: 0, a: 0.1, d: 0, s: 1, r: 0.8)
-  mul = Mul(sources: @[env.UG, saw1.UG])
-  mix = Mix(sources: @[mul.UG], amp: 0.2)
+  mix1 = Mix(sources: @[saw1.UG, saw2.UG], amp: 1)
+  mul = Mul(sources: @[env.UG, mix1.UG])
+  mix2 = Mix(sources: @[mul.UG], amp: 0.2)
 
-ss.stop(ss.start(mix))
+ss.stop(ss.start(mix2))
