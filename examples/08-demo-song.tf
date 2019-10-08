95b5de3b7df5617dce437e39990493666c1e5e98
t-sin <shinichi.tanaka45@gmail.com>
2019-10-09 01:15:03 +0900

    Example: demo song but very discord

---
+++ examples/08-demo-song.tf
@@ -0,0 +1,135 @@
+% %%
+% %% Title: A Song for Altair
+% %% Author: t-sin
+% %% Status: WIP
+% %%
+
+% %% Why output sound are unstable pitch? WHY????
+
+
+% %%
+% %% first track
+
+440 sin              % oscillator
+dup
+0.05 0.1 0.3 0.05 adsr  % envelope
+dup
+rot swap
+
+% sequence 1
+()
+:d 3 2 n :r 4 -2 n :a 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+:d 3 2 n :r 4 -2 n :a 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+
+:c 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+:c 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+
+:a+ 2 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :f 2 2 n :r 4 -2 n :a+ 3 2 n :r 4 -2 n
+:a+ 2 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :f 2 2 n :r 4 -2 n :a+ 3 2 n :r 4 -2 n
+
+:a 3 2 n :r 4 -2 n :a 2 2 n :r 4 -2 n :e 3 2 n :r 4 -2 n :a 2 2 n :r 4 -2 n
+:a 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :e 3 2 n :r 4 -2 n
+
+:d 3 2 n :r 4 -2 n :a 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+:d 3 2 n :r 4 -2 n :a 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+
+:c 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+:c 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+
+:a+ 2 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :f 2 2 n :r 4 -2 n :a+ 3 2 n :r 4 -2 n
+:a+ 2 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :f 2 2 n :r 4 -2 n :a+ 3 2 n :r 4 -2 n
+
+:a 3 2 n :r 4 -2 n :a 2 2 n :r 4 -2 n :e 3 2 n :r 4 -2 n :a 2 2 n :r 4 -2 n
+:a 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :e 3 2 n :r 4 -2 n
+
+:d 3 2 n :r 4 -2 n :a 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+:d 3 2 n :r 4 -2 n :a 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+
+:c 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+:c 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :b 3 2 n :r 4 -2 n
+
+:a+ 2 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :f 2 2 n :r 4 -2 n :a+ 3 2 n :r 4 -2 n
+:a+ 2 2 n :r 4 -2 n :d 3 2 n :r 4 -2 n :f 2 2 n :r 4 -2 n :a+ 3 2 n :r 4 -2 n
+
+:a 3 2 n :r 4 -2 n :a 2 2 n :r 4 -2 n :e 3 2 n :r 4 -2 n :a 2 2 n :r 4 -2 n
+:a 3 2 n :r 4 -2 n :g 3 2 n :r 4 -2 n :f+ 3 2 n :r 4 -2 n :e 3 2 n :r 4 -2 n
+
+:a 3 4 n
+seq
+() swap append
+
+
+% %%
+% %% second track
+
+440 sin              % oscillator
+swap over
+0.05 0.1 0.3 0.05 adsr  % envelope
+swap over
+
+% sequence 2
+()
+:a 3 4 n :e 3 4 n :a 3 4 n :e 3 4 n
+:g 3 4 n :e 3 4 n :g 3 4 n :e 3 4 n
+:f 3 4 n :d 3 4 n :f 3 4 n :d 3 4 n
+:c+ 3 4 n :e 3 4 n :a 2 4 n :e 3 4 n
+
+:a 3 4 n :e 3 4 n :a 3 4 n :e 3 4 n
+:g 3 4 n :e 3 4 n :g 3 4 n :e 3 4 n
+:f 3 4 n :d 3 4 n :f 3 4 n :d 3 4 n
+:c+ 3 4 n :e 3 4 n :a 2 4 n :e 3 4 n
+
+:a 3 4 n :e 3 4 n :a 3 4 n :e 3 4 n
+:g 3 4 n :e 3 4 n :g 3 4 n :e 3 4 n
+:f 3 4 n :d 3 4 n :f 3 4 n :d 3 4 n
+:c+ 3 4 n :e 3 4 n :a 2 4 n :e 3 4 n
+:f+ 3 4 n
+seq
+swap rot swap append
+
+
+% %%
+% %% third track
+440 saw              % oscillator
+swap over
+0.05 0 0.8 0.05 adsr  % envelope
+swap over
+
+% sequence 3
+()
+:r 3 -6 n :r 3 -6 n :r 3 -6 n :r 3 -6 n :r 3 -6 n
+
+:f+ 3 5 n :r 3 -4 n
+:e 3 2 n :r 3 -2 n :f+ 3 2 n :r 3 -2 n :g 3 5 n :r 3 -4 n
+:f+ 3 2 n :r 3 -2 n :g 3 2 n :r 3 -2 n :a 3 5 n :r 3 -4 n
+:a 3 2 n :r 3 -2 n :a+ 3 2 n :r 3 -2 n :a 3 5 n :r 3 -4 n :r 3 -4 n
+
+:f+ 3 5 n :r 3 -4 n
+:e 3 2 n :r 3 -2 n :f+ 3 2 n :r 3 -2 n :g 3 5 n :r 3 -4 n
+:f+ 3 2 n :r 3 -2 n :g 3 2 n :r 3 -2 n :a 3 4 n :d 3 4 n :r 3 -4 n
+:a 3 2 n :r 3 -2 n :g 3 2 n :f+ 3 2 n :e 3 5 n :r 3 -4 n :r 3 -4 n
+
+:c 3 5 n
+seq
+
+swap rot swap append
+
+
+% %%
+% %% setup
+
+% set sequencers
+ev
+
+% multiply each pair of oscillator and envelope
+() swap append swap append mul
+swap
+() swap append swap append mul
+rot
+() swap append swap append mul
+
+% mix three tracks
+() swap append swap append swap append 0.2 mix
+
+% set unit generators
+ug
